a4a61da4d1f2fe0ababac3fb50e698645250864a
Julian <V_Jule@web.de>
2019-11-19 16:44:38 +0100

    All complete

--- processing.bat
+++ processing.bat
@@ -1,19 +1,7 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf830
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Calibri;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww16140\viewh8400\viewkind0
-\deftab529
-\pard\pardeftab529\ri-386\partightenfactor0
+ogr2ogr -s_srs EPSG:4326 -t_srs EPSG:32632 gadm36_DEU_2_UTM.shp gadm36_DEU_2.shp
+ogr2ogr -t_srs EPSG:32632 motorway_UTM.geojson motorway.geojson
+ogr2ogr -t_srs EPSG:32632 forest_UTM.geojson forest.geojson
 
-\f0\fs22 \cf0 ogr2ogr -where NAME_2="Mannheim" Mannheim.geojson gadm36_DEU_2.shp\
-ogr2ogr -where NAME_2="Heidelberg" Heidelberg.geojson gadm36_DEU_2.shp
-\f1\fs24 \
 
-\f0\fs22 ogr2ogr -where NAME_2="Bergstra\'dfe" Bergstra\'dfe.geojson gadm36_DEU_2.shp
-\f1\fs24 \
-
-\f0\fs22 ogr2ogr -where NAME_2="Rhein-Neckar-Kreis" Rhein-Neckar-Kreis.geojson\
-gadm36_DEU_2.shp
-\f1\fs24 \
-}
+ogr2ogr -f GeoJSON -t_srs EPSG:32632 gadm36_DEU_2_UTM.geojson gadm36_DEU_2_UTM.shp
+ogr2ogr -where "NAME_2 in ('Bergstraße','Heidelberg','Mannheim','Rhein-Neckar-Kreis')" districts.geojson gadm36_DEU_2_UTM.geojson
